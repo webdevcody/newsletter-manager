929cd41d3afe39d716ce61a7dcc024bde2d74895
Web Dev Cody <webdevcody@gmail.com>
2023-05-04 12:35:31 -0400

    remove cloudfront from deployment

--- infra/ui.tf
+++
@@ -1,6 +0,0 @@
-module "cloudfront_s3_website_with_domain" {
-    source                 = "./site"
-    domain_name            = "newsletter.webdevcody.com"
-    acm_certificate_domain = "*.webdevcody.com"
-    upload_sample_file     = true    
-}
